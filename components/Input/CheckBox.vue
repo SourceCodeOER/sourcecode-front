11294cbad047c56d49ea468d4139ac616e08fbd2
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-11-08 21:59:18 +0100

    feat: add CheckBox with typescript

---
+++ components/Input/CheckBox.vue
@@ -0,0 +1,102 @@
+<template>
+  <label>
+    {{title}}
+    <input type="checkbox" v-model="checked">
+    <span class="checkmark"></span>
+  </label>
+</template>
+
+<script lang="ts">
+    import {Vue, Component, Prop, Emit, Watch} from 'vue-property-decorator'
+
+    @Component
+    export default class CheckBox extends Vue {
+
+        checked = false;
+
+        @Prop(String) readonly title!: string;
+        @Prop(Boolean) readonly state!: boolean;
+        @Prop({type: [String, Number], default: 'none'}) readonly id!: string | number;
+
+        @Emit()
+        check(): { state: boolean; id: string | number; title: string } {
+            return {title: this.title, state: this.checked, id: this.id}
+        }
+
+        @Watch('checked')
+        @Emit('check')
+        onStateChanged(val: boolean) {
+            return {title: this.title, state: val, id: this.id}
+        }
+
+        created() {
+            this.checked = this.state;
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import "./../../assets/css/_variables";
+  @import "./../../assets/css/_function";
+  @import "./../../assets/css/_font";
+
+  label {
+    position: relative;
+    display: block;
+    line-height: 2em;
+    padding-left: 35px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:hover input ~ .checkmark {
+      background-color: #ccc;
+    }
+
+    input {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+      height: 0;
+      width: 0;
+
+      &:checked ~ .checkmark {
+        background-color: $TERNARY_COLOR;
+      }
+
+      &:checked ~ .checkmark:after {
+        display: block;
+        background-color: $TERNARY_COLOR;
+      }
+    }
+
+    .checkmark:after {
+      left: 9px;
+      top: 5px;
+      width: 5px;
+      height: 10px;
+      border: solid white;
+      border-width: 0 3px 3px 0;
+      -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      transform: rotate(45deg);
+    }
+  }
+
+  .checkmark {
+    position: absolute;
+    top: 50%;
+    @include transformHelper(translateY(-50%));
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+
+    &:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
+  }
+</style>
