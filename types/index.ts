8d3cf8d65b0e59ecd060fe4873cac91281b91826
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-11-13 22:33:20 +0100

    feat(types): add types for Tags and Categories

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,38 @@
 
 export interface CheckBoxObjectEmitted {
-  title:string,
+  text:string,
   state:boolean,
   id: number
 }
 
 export interface Tag {
   id: number,
-  title: string
+  text: string
+}
+
+export interface Category {
+  id: number,
+  category: string
+}
+
+type DEACTIVATED = 0
+type ACTIVE = 1
+type PENDING = 2
+type INACTIVE = 3
+
+export type SelectedTag = {
+  id: number,
+  text: string,
+  state: DEACTIVATED | ACTIVE | PENDING | INACTIVE,
+  category: number
+}
+
+export type TagsCategory = {
+  id:number,
+  category: string,
+  tags: Tag[]
+}
+
+export interface ExtendedTag extends TagsCategory{
+  tags: SelectedTag[]
 }
