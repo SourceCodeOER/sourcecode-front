a58baed5cca0703e11246656e51409c8be0ed4af
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-05 10:11:02 +0100

    feat(index): remember the previous search bar request

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="banner search-banner">
       <div class="input-wrapper">
         <SearchSymbol/>
-        <input type="text" v-on:input="debounceInput" placeholder="Rechercher">
+        <input ref="inputText" type="text" v-on:input="debounceInput" placeholder="Rechercher">
       </div>
     </div>
 
@@ -18,51 +18,58 @@
 </template>
 
 <script lang="ts">
-    import FilterPanel from "~/components/Panel/FilterPanel.vue";
-    import HistoricalPanel from '~/components/Panel/HistoricalPanel.vue'
-    import ExercisesPanel from "~/components/Panel/ExercisesPanel.vue";
-    import SearchSymbol from "~/components/Symbols/SearchSymbol.vue";
-    import {Component, Vue} from "vue-property-decorator";
-    import {BusEvent} from '~/components/Event/BusEvent'
+  import FilterPanel from "~/components/Panel/FilterPanel.vue";
+  import HistoricalPanel from '~/components/Panel/HistoricalPanel.vue'
+  import ExercisesPanel from "~/components/Panel/ExercisesPanel.vue";
+  import SearchSymbol from "~/components/Symbols/SearchSymbol.vue";
+  import {Component, Ref, Vue} from "vue-property-decorator";
+  import {BusEvent} from '~/components/Event/BusEvent'
 
-    const debounce = require('lodash.debounce');
+  const debounce = require('lodash.debounce');
 
-    const FILTER_PANEL = 0;
-    const HISTORICAL_PANEL = 1;
-    const FAVORITE_PANEL = 2;
+  const FILTER_PANEL = 0;
+  const HISTORICAL_PANEL = 1;
+  const FAVORITE_PANEL = 2;
 
-    @Component({
-        components: {
-            FilterPanel,
-            ExercisesPanel,
-            SearchSymbol,
-            HistoricalPanel
-        },
-        async fetch({app: {$accessor}}) {
-            await $accessor.tags.fetch();
-            await $accessor.tags.apply();
-            await $accessor.search.fetch({});
-        }
-    })
-    export default class extends Vue {
-        currentAsidePanel: 0 | 1 | 2 = FILTER_PANEL;
-
-        private changePanel(id: 0 | 1 | 2) {
-            this.currentAsidePanel = id
-        }
-
-        debounceInput = debounce((e: any) => {
-            this.$accessor.search.fetch({data: {title: e.target.value}})
-        }, 250);
-
-        beforeDestroy() {
-            BusEvent.$off('changePanel', this.changePanel)
-        }
-
-        created() {
-            BusEvent.$on('changePanel', this.changePanel)
-        }
+  @Component({
+    components: {
+      FilterPanel,
+      ExercisesPanel,
+      SearchSymbol,
+      HistoricalPanel
+    },
+    async fetch({app: {$accessor}}) {
+      await $accessor.tags.fetch();
+      await $accessor.tags.apply();
+      await $accessor.search.fetch({});
     }
+  })
+  export default class extends Vue {
+    currentAsidePanel: 0 | 1 | 2 = FILTER_PANEL;
+
+    @Ref() inputText!: HTMLInputElement;
+
+    private changePanel(id: 0 | 1 | 2) {
+      this.currentAsidePanel = id
+    }
+
+    debounceInput = debounce((e: any) => {
+      this.$accessor.search.fetch({data: {title: e.target.value}})
+    }, 250);
+
+    beforeDestroy() {
+      BusEvent.$off('changePanel', this.changePanel)
+    }
+
+    mounted() {
+      const title = this.$accessor.search.search_criterion.title;
+      this.inputText.value = !!title ? title : ''
+    }
+
+    created() {
+      BusEvent.$on('changePanel', this.changePanel)
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
