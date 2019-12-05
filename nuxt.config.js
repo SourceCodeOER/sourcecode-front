c5c81f8999f9aa9dc8b75e7a2d814241e97d1d67
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-05 10:35:16 +0100

    feat(style): add font for code syntax

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Montserrat:200,400,700&display=swap'
+        href: 'https://fonts.googleapis.com/css?family=Montserrat:200,400,700&Source+Code+Pro&display=swap'
       }
     ],
     script: [
