26442046491a3ddb99c281cfca3cce33a36a27e7
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-05 09:26:20 +0100

    feat(exercise): create custom template for one exercise

--- pages/exercices/_id.vue
+++ pages/exercices/_id.vue
@@ -2,7 +2,7 @@
   <div class="container" id="Exercise">
     <div class="banner">
       <span>
-        The Weighted Set Cover Problem
+        {{exercise.title}}
       </span>
       <nuxt-link to="/" tag="span">
         <ArrowSymbol class="reversed-arrow" theme="theme--secondary-color"/>
@@ -13,23 +13,22 @@
     <div class="wrapper">
       <aside class="panel">
         <h3>Détails</h3>
-        <h3>Sources</h3>
+
+        <div v-for="item in tag_by_categories" :key="item.category">
+          <h4>{{item.category}}</h4>
+          <ul>
+            <li v-for="tag in item.tags" :key="tag + '_' + item.category">{{tag}}</li>
+          </ul>
+        </div>
       </aside>
 
-      <section>
-        <h1>The Weighted Set Cover Problem</h1>
+      <section class="exercise">
+        <h1>{{exercise.title}}</h1>
+        <span>Créé le {{$moment(exercise.createdAt).format("DD/MM/YY à H:mm")}}</span> | <span>Mis à jour le {{$moment(exercise.updatedAt).format("DD/MM/YY à H:mm")}}</span>
 
         <h2>Description</h2>
 
-        <article>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores aspernatur debitis eaque exercitationem, expedita, fuga id, impedit molestiae molestias natus necessitatibus nisi nostrum omnis quidem? Ab iste recusandae veritatis?
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consectetur consequatur deserunt dicta ducimus eius molestias, neque odit omnis quia quod repellat rerum voluptate! Accusamus atque consectetur dolorum facilis nemo?
-          </p>
-        </article>
+        <article v-html="exercise.description"></article>
       </section>
     </div>
   </div>
@@ -38,14 +37,59 @@
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
     import ArrowSymbol from "~/components/Symbols/ArrowSymbol.vue";
+    import {Exercise, TagExercise} from "~/types";
 
     @Component({
         components: {
             ArrowSymbol
+        },
+        async asyncData({params, error, app: {$axios}}) {
+            const id = params.id;
+
+            try {
+                const exercise: Exercise = await $axios.$get(`api/exercises/${id}`);
+                const {data} = await $axios.$post('intern-api/exercise-converter', {raw: exercise.description});
+
+                exercise.description = data;
+                return {exercise}
+            } catch (e) {
+                error({statusCode: 404, message: "Cette exercice est introuvable"});
+            }
+
+
         }
     })
     export default class extends Vue {
+        exercise!: Exercise;
 
+        get tag_by_categories() {
+            const map: Map<string, string[]> = new Map();
+
+            this.exercise.tags.forEach((tag: TagExercise) => {
+                const el: string[] | undefined = map.get(tag.category.category_text);
+                if (el !== undefined) {
+                    el.push(tag.tag_text)
+                } else {
+                    map.set(tag.category.category_text, [tag.tag_text])
+                }
+            });
+
+            const entries = (new Map([...map.entries()].sort((a, b) => a[0] > b[0] ? 1 : -1))).entries();
+            new Map([...map.entries()].sort((a, b) => a[0] > b[0] ? 1 : -1));
+            let tag_with_categories = entries.next();
+
+            const arrayOfTagByCategories: { category: string, tags: string[] }[] = [];
+            while (!tag_with_categories.done) {
+                const value = tag_with_categories.value;
+                arrayOfTagByCategories.push({category: value[0], tags: value[1]});
+                tag_with_categories = entries.next()
+            }
+
+            return arrayOfTagByCategories
+        }
+
+        mounted() {
+        }
     }
 </script>
 
@@ -53,6 +97,7 @@
 
   @import "../../assets/css/_variables";
   @import "../../assets/css/_function";
+  @import "../../assets/css/_font";
 
   #Exercise {
 
@@ -85,22 +130,45 @@
       }
     }
 
+    .panel {
+
+      h4 {
+        text-transform: capitalize;
+        color:$TERNARY_COLOR;
+        font-family: $CircularStd;
+        margin-top:10px;
+        margin-bottom:10px;
+      }
+
+      ul {
+        list-style-type: none;
+        padding: 0;
+        font-weight: 300;
+      }
+
+      li:not(:last-child) {
+        margin-bottom: 5px;
+      }
+    }
+
     section {
       width: calc(100% - #{$FILTER_PANEL_WIDTH + $PADDING_CONTENT});
       margin-left: auto;
       height: 100%;
-      background-color:white;
+      background-color: white;
       @include box-shadow($SHADOW);
       border-radius: 4px;
-
+      margin-bottom:30px;
       padding: 20px;
 
       h1 {
-        margin-bottom: 20px;
+        margin-bottom: 5px;
       }
 
       h2 {
-        color:$SECONDARY_COLOR;
+        color: $SECONDARY_COLOR;
+        margin-bottom: 10px ;
+        margin-top: 30px ;
       }
 
       article {
