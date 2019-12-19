da9f3f840aa312b68d65239f5f11ab545d4a4edb
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-19 10:39:58 +0100

    feat(Symbols): add new svg symbols
    - Check
    - Trash
    - Document
    - Close

---
+++ components/Symbols/DocumentSymbol.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path :class="theme" d="M288 48H136c-22.092 0-40 17.908-40 40v336c0 22.092 17.908 40 40 40h240c22.092 0 40-17.908 40-40V176L288 48zm-16 144V80l112 112H272z"/></svg>
+</template>
+
+<script lang="ts">
+    import Symbol from "./Symbol.vue";
+    import {Component, Mixins} from 'vue-property-decorator'
+
+    @Component
+    export default class DocumentSymbol extends Mixins(Symbol) {}
+</script>
---
+++ components/Symbols/TrashSymbol.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path :class="theme" d="M128 405.429C128 428.846 147.198 448 170.667 448h170.667C364.802 448 384 428.846 384 405.429V160H128v245.429zM416 96h-80l-26.785-32H202.786L176 96H96v32h320V96z"/></svg>
+</template>
+
+<script lang="ts">
+  import Symbol from "./Symbol.vue";
+  import {Component, Mixins} from 'vue-property-decorator'
+
+  @Component
+  export default class TrashSymbol extends Mixins(Symbol) {}
+</script>
