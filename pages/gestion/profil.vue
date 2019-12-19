c2106de5438f0a0ee1a7f5da53b9a58c35ccd11f
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-19 10:41:51 +0100

    feat(Gestion): add new page to view our profile

---
+++ pages/gestion/profil.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container--with-menu">
+    <div class="banner banner--nav banner--with-shadow-bottom">
+      <span>
+        Gestion > Mon profil
+      </span>
+      <nuxt-link to="/exercices" tag="span">
+        <ArrowSymbol class="reversed-arrow" theme="theme--secondary-color"/>
+        Retour
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+  import ArrowSymbol from "~/components/Symbols/ArrowSymbol.vue";
+
+  @Component({
+    components: {
+      ArrowSymbol
+    }
+  })
+  export default class extends Vue {
+
+  }
+</script>
