4ef360aef876f81f82e81a14cbae3be0a7543040
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-22 12:55:36 +0100

    feat(types): add declaration of tiptap and hightlightjs with languages

--- ts-shim.d.ts
+++ ts-shim.d.ts
@@ -2,3 +2,12 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+
+declare module 'tiptap';
+declare module 'tiptap-extensions';
+declare module 'highlight.js/lib/languages/javascript';
+declare module 'highlight.js/lib/languages/css';
+declare module 'highlight.js/lib/languages/java';
+declare module 'highlight.js/lib/languages/python';
+declare module 'highlight.js/lib/languages/cmake';
+declare module 'highlight.js/lib/languages/cs';
