e43f30b96a285b05832400a08dab0a6fd68de30a
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-22 12:59:27 +0100

    feat(Symbols): add plus symbol

---
+++ components/Symbols/PlusSymbol.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path :class="theme" d="M416 277.333H277.333V416h-42.666V277.333H96v-42.666h138.667V96h42.666v138.667H416v42.666z"/></svg>
+</template>
+
+<script lang="ts">
+  import Symbol from "./Symbol.vue";
+  import {Component, Mixins} from 'vue-property-decorator'
+
+  @Component
+  export default class PlusSymbol extends Mixins(Symbol) {}
+</script>
