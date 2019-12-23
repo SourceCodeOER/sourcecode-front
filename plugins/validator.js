e14836f0e4099ae4046b076cf38fedbb9689f19d
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-23 11:18:27 +0100

    feat(plugins): add new validation rule (validator)

--- plugins/validator.js
+++ plugins/validator.js
@@ -2,13 +2,14 @@ import {localize} from 'vee-validate';
 import fr from 'vee-validate/dist/locale/fr.json'
 
 import {extend} from 'vee-validate';
-import {required, email, alpha_spaces, max, numeric} from 'vee-validate/dist/rules';
+import {required, email, alpha_spaces, max, numeric, regex} from 'vee-validate/dist/rules';
 
 extend('required', required);
 extend('email', email);
 extend('alpha_spaces', alpha_spaces);
 extend('max', max);
 extend('numeric', numeric);
+extend('regex', regex);
 
 
 export default function ({app}) {
