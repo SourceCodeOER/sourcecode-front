d1bc0b237fafacda02e406f0fe113047fe142420
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-28 15:16:12 +0100

    feat(ExerciseCheckPanel): add link to the exercise

--- components/Panel/ExercisesCheckPanel.vue
+++ components/Panel/ExercisesCheckPanel.vue
@@ -13,6 +13,10 @@
           <h4>{{exercise.title}}</h4>
           <span><i>Nb de votes</i> : {{exercise.metrics.votes}}</span>
           <span><i>Moyenne</i> : {{exercise.metrics.avg_vote ? exercise.metrics.avg_vote : '-'}}</span>
+          <a :href="`/exercices/${exercise.id}`" target="_blank" class="cta-link">
+            Voir
+            <Icon type="arrow" theme="theme--secondary-color"/>
+          </a>
         </li>
       </ul>
     </div>
@@ -22,8 +26,11 @@
 <script lang="ts">
   import {Component, Prop, Vue} from "vue-property-decorator";
   import {Exercise} from "~/types";
+  import Icon from "~/components/Symbols/Icon.vue";
 
-  @Component
+  @Component({
+    components: {Icon}
+  })
   export default class ExercisesCheckPanel extends Vue {
     @Prop({type: String, default: "Exercices"}) title!: string;
 
@@ -36,6 +43,7 @@
 
 <style lang="scss" scoped>
   @import "../../assets/css/_variables.scss";
+  @import "../../assets/css/_function.scss";
 
   #ExercisesCheckPanel {
     padding: 20px 0;
@@ -79,6 +87,22 @@
       li {
         border-bottom: 1px solid rgba($GREY, .1);
         padding: 10px 20px;
+        position: relative;
+
+        .cta-link {
+          position: absolute;
+          opacity: 0;
+          bottom: 5px;
+          right: 5px;
+          color:$SECONDARY_COLOR;
+          text-decoration: none;
+          font-weight: bold;
+          @include transitionHelper(opacity .4s ease);
+          svg, img {
+            margin-left: 3px;
+            width: 20px;
+          }
+        }
 
         span {
           display: block;
@@ -86,6 +110,10 @@
 
         &:hover {
           background-color: rgba($GREY, .1);
+
+          .cta-link {
+            opacity: 1;
+          }
         }
 
       }
