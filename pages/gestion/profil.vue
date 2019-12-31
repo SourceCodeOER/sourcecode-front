ad3ebeb35a8a8fcbc34fa685305ba91d75090cb4
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2019-12-31 13:48:56 +0100

    feat(Profile): add profile description

--- pages/gestion/profil.vue
+++ pages/gestion/profil.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container--with-menu">
-    <div class="banner banner--nav banner--with-shadow-bottom">
+  <div class="container--with-menu" id="Profile">
+    <div class="banner banner__nav banner--with-shadow-bottom">
       <span>
         Gestion > Mon profil
       </span>
-      <nuxt-link to="/exercices" tag="span">
-        <Icon type="arrow" class="reversed-arrow" theme="theme--secondary-color"/>
-        Retour
-      </nuxt-link>
+    </div>
+
+    <div class="wrapper">
+      <div class="content">
+        <h1>Mon profil</h1>
+
+        <div class="profile__item">
+          <h2 class="title--secondary-color">Rôle</h2>
+          {{user.role}}
+        </div>
+        <div class="profile__item">
+          <h2 class="title--secondary-color">Adresse mail</h2>
+          {{user.email}}
+        </div>
+        <div class="profile__item">
+          <h2 class="title--secondary-color">Nom complet</h2>
+          {{user.fullName}}
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
@@ -15,13 +31,49 @@
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator'
   import Icon from "~/components/Symbols/Icon.vue";
+  import {UserInfo} from "~/types";
 
   @Component({
     components: {
       Icon
+    },
+    async asyncData({$auth}) {
+      return {user: $auth.user}
     }
   })
   export default class extends Vue {
+    user!: UserInfo;
 
+    mounted() {
+      console.log(this.user)
+    }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import "../../assets/css/_variables";
+  @import "../../assets/css/_function";
+
+  #Profile {
+
+    h1 {
+      margin-bottom: 20px;
+    }
+
+    .content {
+      background-color: white;
+      border-radius: 4px;
+      @include box-shadow($SHADOW);
+      padding: 20px;
+
+    }
+
+    .profile__item {
+      h2 {
+        margin-bottom: 5px;
+      }
+
+      margin-bottom: 20px;
+    }
+  }
+</style>
