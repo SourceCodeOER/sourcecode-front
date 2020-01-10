607da91e7660243bad23df04423f390598717a41
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-01-10 22:17:31 +0100

    style(middleware): reset all the search request, not just the search criterion

--- middleware/reset-search-request.ts
+++ middleware/reset-search-request.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@nuxt/types'
 
 const middleware: Middleware = ({app}) => {
-    app.$accessor.search.RESET_SEARCH_CRITERION();
+    app.$accessor.search.RESET();
     app.$accessor.tags.CLEAR();
 };
 
