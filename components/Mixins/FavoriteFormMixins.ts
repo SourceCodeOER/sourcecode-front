88ad6f04e03342aa8d9e1ce078f736c433713df6
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-01-10 22:10:12 +0100

    feat(FavoriteFormMixins): add vote support

--- components/Mixins/FavoriteFormMixins.ts
+++ components/Mixins/FavoriteFormMixins.ts
@@ -1,6 +1,6 @@
 import {Component, Vue, Ref} from "vue-property-decorator";
 import {ValidationObserver} from "vee-validate";
-import {CreateConfigurationRequest, SelectedTag} from "~/types";
+import {CreateConfigurationRequest, SelectedTag, TagLabelObjectEmitted} from "~/types";
 const debounce = require('lodash.debounce');
 
 
@@ -12,10 +12,39 @@ export default class extends Vue {
     title:'',
     name: '',
     tags: []
-  }
+  };
 
   get selectedTags(): SelectedTag[] {
     return this.$accessor.tags.selectedTags
+  }
+
+  async deleteTag(event: { title: string, id: number, state: boolean }, tag: SelectedTag) {
+    await this.$accessor.tags.addOrRemoveTag({...tag, state: event.state});
+    await this.$accessor.tags.apply('default');
+    await this.$accessor.search.fetch({data: {tags: this.$accessor.tags.tagsRequest}});
+  }
+
+  get voteTag(): TagLabelObjectEmitted | undefined {
+    const vote = this.$accessor.search.search_criterion.vote;
+    let title = '';
+
+    if (vote) {
+      if (vote.operator === '>=' || vote.operator === '>') {
+        title += 'Plus de '
+      } else if (vote.operator === '<=' || vote.operator === '<') {
+        title += 'Moins de '
+      }
+
+      title += vote.value + ' étoiles'
+
+      return {title, id: 0, state: true}
+    }
+    return undefined
+  }
+
+  async removeRatingCriteria() {
+    await this.$accessor.search.RESET_VOTE();
+    await this.$accessor.search.fetch({});
   }
 
   /**
