af83dac895e01f5ec2588dd4294304e3c92b9e78
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-01-10 22:25:15 +0100

    refactor(Favorite): replace form template by the FavoriteForm component

--- pages/gestion/mes-favoris/creer-favori.vue
+++ pages/gestion/mes-favoris/creer-favori.vue
@@ -16,147 +16,44 @@
 
       <Panel>
         <PanelItem>
-          <ExercisesCheckPanel title="Exercices"/>
+          <ExercisesCheckPanel icon="search" title="Exercices"/>
         </PanelItem>
 
         <PanelItem>
-          <FilterPanel :reset-button="true" :search-mode="true" mode="strict" title="Tags"/>
+          <FilterPanel :radio-button-rating="true" :reset-button="true" :search-mode="true" title="Tags"/>
         </PanelItem>
       </Panel>
 
-      <section class="content">
-        <h1>Créer un favori</h1>
-
-        <ValidationObserver ref="observer1" tag="form" @submit.prevent="validateBeforeSubmit()">
-          <ValidationProvider tag="label"
-                              name="favori"
-                              rules="required|max:200"
-                              v-slot="{ errors }">
-            <span class="label__name">
-              Nom du favori *
-            </span>
-            <input id="Name" placeholder="Entrez le nom de votre favori" name="name" v-model="form.name"
-                   class="input--grey" type="text">
-            <span class="error-message">{{errors[0]}}</span>
-          </ValidationProvider>
-
-          <ValidationProvider tag="label"
-                              name="titre"
-                              rules="max:100"
-                              v-slot="{ errors }">
-            <span class="label__name">
-              Titre de la sélection
-            </span>
-            <input id="Title" placeholder="Entrez le titre de votre recherche" name="title" v-model="form.title"
-                   v-on:input="debounceInput" class="input--grey" type="text">
-            <span class="error-message">{{errors[0]}}</span>
-          </ValidationProvider>
-
-        </ValidationObserver>
-
-        <h2 class="title--primary-color__light">Tags *</h2>
-        <button class="button--secondary-color-reverse" v-show="selectedTags.length === 0" @click="changePanel(1)">
-          Commencer la sélection
-        </button>
-
-
-        <Tag v-for="(tag, id) in selectedTags" :store-mode="true" :search-mode="true" mode="strict" :state="tag.state"
-             :title="tag.tag_text"
-             :key="tag.tag_text + '_' + tag.category + '_'+ id"
-             :category="tag.category_id" :id="tag.tag_id"/>
-
-        <br>
-
-        <button @click="validateBeforeSubmit" class="button--ternary-color-reverse button__validate">
-          sauver le favori
-        </button>
-        <p class="disclaimer">* champs obligatoires</p>
-      </section>
+      <FavoriteForm title="Créer un favori"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Mixins} from "vue-property-decorator";
-  import {
-    CreateConfigurationRequest,
-    SelectedTag,
-  } from "~/types";
+  import {Component, Vue} from "vue-property-decorator";
   import ExercisesCheckPanel from "~/components/Panel/Item/ExercisesCheckPanel.vue";
   import FilterPanel from "~/components/Panel/Item/FilterPanel.vue";
-  import FilterPanelMixins from "~/components/Mixins/FilterPanelMixins.vue";
   import Icon from "~/components/Symbols/Icon.vue";
-  import Tag from "~/components/Tag/Tag.vue";
-  import {ValidationObserver, ValidationProvider} from "vee-validate";
-  import {BusEvent} from "~/components/Event/BusEvent";
   import Panel from "~/components/Panel/Panel.vue";
   import PanelItem from "~/components/Panel/PanelItem.vue";
-  import FavoriteFormMixins from "~/components/Mixins/FavoriteFormMixins";
+  import FavoriteForm from "~/components/Gestion/FavoriteForm.vue";
 
   @Component({
     components: {
+      FavoriteForm,
       PanelItem,
       Panel,
       ExercisesCheckPanel,
       FilterPanel,
-      Icon,
-      ValidationObserver,
-      ValidationProvider,
-      Tag
+      Icon
     },
     async fetch({app: {$accessor}}) {
       await $accessor.tags.fetch();
+      $accessor.search.UPDATE_INCLUDE_OPTIONS({includeDescription: false, includeTags: false});
+      $accessor.search.UPDATE_ORDER_BY([{field: "date", value: "DESC"}, {field: 'id', value: 'ASC'}])
     },
-    middleware: ['reset-search-request']
+    middleware: ['auth', 'reset-search-request']
   })
-  export default class extends Mixins(FilterPanelMixins, FavoriteFormMixins) {
-    async validateBeforeSubmit() {
-      // Basic validation form
-      const isValid = await this.observer1.validate();
-
-      // Tags validation
-      const tags: number[] = this.selectedTags.map((tag: SelectedTag) => tag.tag_id);
-      const isTagsValid = tags.length !== 0;
-
-      if (isValid && isTagsValid) {
-
-        try {
-
-          const request: CreateConfigurationRequest = {
-            name: this.form.name,
-            tags
-          };
-
-          if (this.form.title !== '') {
-            request.title = this.form.title
-          }
-
-          await this.$axios.$post('api/configurations', request);
-
-          await this.$accessor.favorites.fetch();
-
-          BusEvent.$emit('displayNotification', {
-            mode: 'success',
-            message: 'Votre favori a bien été créé !'
-          })
-
-        } catch (e) {
-          BusEvent.$emit('displayNotification', {
-            mode: 'error',
-            message: 'Une erreur est survenue lors de la création du favori.'
-          })
-        }
-      } else {
-        BusEvent.$emit('displayNotification', {
-          mode: 'warning',
-          message: 'Vous ne respectez pas les conditions pour ajouter votre favori.'
-        })
-      }
-    }
-
+  export default class extends Vue {
   }
 </script>
-
-<style lang="scss" scoped>
-  @import "../../../assets/css/_exercise-gestion.scss";
-</style>
