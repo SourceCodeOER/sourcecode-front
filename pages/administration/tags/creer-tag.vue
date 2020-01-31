9d5569a79646b7fd342615925583259c41615599
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-01-31 14:21:31 +0100

    feat(Admin): close #14

---
+++ pages/administration/tags/creer-tag.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="container--with-menu">
+    <div class="banner banner--with-shadow-bottom">
+      <div class="banner__nav banner__nav--with-link">
+        <span>
+          Administration > Gestion des tags > Créer un tag
+        </span>
+        <nuxt-link to="/administration/tags" tag="span">
+          <Icon type="arrowLeft" class="reversed-arrow" theme="theme--primary-color-light"/>
+          Gestion des tags
+        </nuxt-link>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <section class="content">
+        <h1>Créer un tag</h1>
+
+        <ValidationObserver ref="observer" tag="form" @submit.prevent="validateBeforeSubmit">
+          <ValidationProvider tag="label"
+                              name="titre"
+                              rules="required|min:1|max:200"
+                              v-slot="{ errors }">
+            <span class="label__name">
+              Titre *
+            </span>
+            <input id="Title" placeholder="Entrez le nom du tag" name="title" v-model="form.title"
+                   class="input--grey" type="text">
+            <span class="error-message">{{errors[0]}}</span>
+          </ValidationProvider>
+
+          <ValidationProvider tag="label"
+                              name="catégorie"
+                              :rules="`required|oneOf:${Array.from(categories.keys())}`"
+                              v-slot="{ errors }">
+            <span class="label__name">
+              Catégorie *
+            </span>
+            <select id="CategoryID" name="categoryID" v-model="selectedNewTag.category_id"
+                    class="input--grey">
+              <option :value="i" v-for="(category, i) in categoriesName">{{category}}</option>
+            </select>
+            <CustomSelect ref="customSelect" legend="choisir une catégorie" @change="chooseCategory"
+                          class="custom-select--secondary-color custom-select-focus--secondary-color"
+                          :options="categoriesName" name="newTags"/>
+            <span class="error-message">{{errors[0]}}</span>
+          </ValidationProvider>
+
+        </ValidationObserver>
+
+        <div class="cta__validate--wrapper">
+          <button @click="validateBeforeSubmit(false)" class="button--yellow-reverse cta__validate">
+            Mettre en attente
+          </button>
+          <button @click="validateBeforeSubmit(true)" class="button--ternary-color-reverse cta__validate">
+            Publier
+          </button>
+        </div>
+        <p class="disclaimer">* champs obligatoires</p>
+
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue, Ref} from 'vue-property-decorator'
+  import {ValidationProvider, ValidationObserver} from "vee-validate";
+  import Icon from "~/components/Symbols/Icon.vue";
+  import {
+    Category, CreateTagRequest,
+    TagProposal
+  } from "~/types";
+  import {BusEvent} from "~/components/Event/BusEvent";
+  import {AxiosError} from "axios";
+  import CustomSelect from "~/components/Input/CustomSelect.vue";
+
+
+  @Component({
+    components: {CustomSelect, Icon, ValidationProvider, ValidationObserver},
+    middleware: ['auth', 'admin', 'reset-search-request'],
+    async fetch({app: {$accessor}}) {
+      await $accessor.tags.fetch();
+    }
+  })
+  export default class extends Vue {
+
+    /**
+     * ValidationObserver for the title
+     */
+    @Ref() observer!: InstanceType<typeof ValidationObserver>;
+
+    /**
+     * Reference of the custom select element attached to the observer3
+     */
+    @Ref() customSelect!: CustomSelect;
+
+    form = {
+      title: ""
+    };
+
+    /**
+     * A new tag proposal
+     */
+    selectedNewTag: TagProposal = {
+      category_id: -1,
+      text: ""
+    };
+
+    /**
+     * Contains all the new tags proposal
+     */
+    newTags: TagProposal[] = [];
+
+    /**
+     * Get categories from the tags store
+     */
+    protected get categories(): Category[] {
+      return this.$accessor.tags.categories
+    }
+
+    /**
+     * Only get the names of each categories
+     */
+    protected get categoriesName(): string[] {
+      return this.categories.map(el => el.category).filter(el => el !== 'licence');
+    }
+
+    /**
+     * Select handler for the customSelect reference
+     * @param event
+     */
+    chooseCategory(event: { content: string, index: number }) {
+      this.selectedNewTag.category_id = event.index;
+    }
+
+    /**
+     * Reset the form for tag proposal
+     */
+    resetTagForm() {
+      this.selectedNewTag.text = "";
+      this.selectedNewTag.category_id = -1;
+      (this.customSelect as any).reset();
+    }
+
+    /**
+     * Reset all the all page and all the observers to start with a fresh form
+     */
+    async resetGeneralForm() {
+      this.resetTagForm();
+
+      this.form.title = "";
+
+      // @ts-ignore
+      this.richTextEditor.reset();
+
+      this.newTags = [];
+    }
+
+    /**
+     * Validate the entire page and send the new exercise
+     */
+    async validateBeforeSubmit(tagState: boolean) {
+
+      // Basic validation form
+      const isValid = await this.observer.validate();
+
+      if (isValid) {
+
+        const tagsSettingsRequest: CreateTagRequest = [{
+          text: this.form.title,
+          category_id: this.selectedNewTag.category_id,
+          isValidated: tagState
+        }];
+
+        try {
+
+          await this.$axios.$post('/api/bulk/create_tags', tagsSettingsRequest);
+
+          BusEvent.$emit('displayNotification', {
+            mode: "success",
+            message: "Le tag a bien été créé."
+          });
+
+          requestAnimationFrame(() => {
+            this.observer.reset();
+          });
+
+        } catch (e) {
+          const error = e as AxiosError;
+
+          const response = error.response;
+
+          if (!response) {
+            BusEvent.$emit('displayNotification', {
+              mode: "error",
+              message: "Une erreur est survenue depuis nos serveurs :("
+            })
+          } else {
+            const status = response.status;
+            if (status >= 500 && status < 600) {
+              BusEvent.$emit('displayNotification', {
+                mode: "error",
+                message: "Une erreur est survenue depuis nos serveurs :("
+              })
+            } else if (status === 401) {
+              BusEvent.$emit('displayNotification', {
+                mode: "error",
+                message: "Vous n'êtes pas autorisé à publier ce tag. Reconnectez-vous !"
+              })
+            } else if (status === 409) {
+              BusEvent.$emit('displayNotification', {
+                mode: "error",
+                message: "ce tag a déjà été créé !"
+              })
+            } else {
+              BusEvent.$emit('displayNotification', {
+                mode: "error",
+                message: "La publication du tag à échoué. Vérifiez le formulaire ou contactez notre support."
+              })
+            }
+          }
+        }
+
+      } else {
+        BusEvent.$emit('displayNotification', {
+          mode: "warning",
+          message: "Vous ne respectez pas les conditions pour la publication de votre exercice.",
+          time: 5000
+        })
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../assets/css/exercise-gestion";
+
+  .wrapper {
+    margin-bottom: 40px;
+  }
+</style>
---
+++ pages/administration/tags/index.vue
@@ -0,0 +1,286 @@
+<template>
+  <div class="container--with-menu container--with-table" id="AdminExercises">
+    <div class="banner banner--with-shadow-bottom">
+
+      <div class="banner__nav">
+      <span>
+        Administration > Gestion des tags
+      </span>
+      </div>
+    </div>
+
+    <div class="wrapper wrapper--with-panel wrapper--with-table">
+
+      <Panel>
+        <PanelItem>
+          <TagFilterPanel
+            :reset-button="true"
+            :search-mode="true"
+            @reset="resetInput"/>
+        </PanelItem>
+      </Panel>
+
+      <section>
+
+        <h1>Gestion des tags</h1>
+
+        <div class="header-wrapper">
+          <div class="input-wrapper--with-icon">
+            <Icon type="search"/>
+            <input ref="inputText" class="input--primary-color" type="text" v-model="searchModel"
+                   placeholder="Rechercher">
+          </div>
+
+          <div class="header-wrapper__buttons">
+
+            <transition name="fade">
+              <CustomSelect v-show="!isSelectedTagsEmpty" :stateless="true" @change="selectAction"
+                            name="moreActions" legend="Plus d'actions"
+                            class="custom-select--primary-color custom-select-focus--primary-color"
+                            :options="['Valider', 'Invalider', 'Supprimer']"/>
+            </transition>
+
+            <nuxt-link to="/administration/tags/creer-tag">
+              <button class="button--ternary-color-reverse button--with-symbol">Créer un tag
+                <Icon type="plus" theme="theme--white"/>
+              </button>
+            </nuxt-link>
+          </div>
+
+        </div>
+
+        <table class="table--with-sticky-header">
+          <thead>
+          <tr>
+            <th class="item-checkbox"></th>
+            <th class="item-left">Nom</th>
+            <th>Catégorie</th>
+            <th>Version</th>
+            <th class="item-centered">Status</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <template v-for="(categoryWithTags, index) in filteredCategoriesWithSearchModel">
+
+            <tr v-for="tag in categoryWithTags.tags" :key="tag.tag_id + '_' + categoryWithTags.category + '_' + index">
+              <td class="item-centered item-checkbox">
+                <CheckBox :state="tag.state" :id="tag.tag_id" @check="addOrRemoveTags($event, tag)"/>
+              </td>
+              <td class="item-left" @click="gotoExercise(1)">{{tag.tag_text}}</td>
+              <td>
+                {{categoryWithTags.category}}
+              </td>
+              <td @click="gotoExercise(1)">{{tag.version}}</td>
+              <td @click="gotoExercise(1)" class="item-centered">
+                <i title="Valide" v-if="tag.isValidated">
+                  <Icon type="check" theme="theme--green"/>
+                </i>
+
+                <i title="Invalide" v-else-if="!tag.isValidated">
+                  <Icon type="close" theme="theme--red-light"/>
+                </i>
+              </td>
+            </tr>
+          </template>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue, Ref} from 'vue-property-decorator'
+  import {
+    CategoryWithSelectedTags,
+    CheckBoxObjectEmitted,
+    SelectedTag, TagExtended
+  } from "~/types";
+  import FilterPanel from "~/components/Panel/Item/FilterPanel.vue";
+  import HistoricalPanel from "~/components/Panel/Item/HistoricalPanel.vue";
+  import FavoritePanel from "~/components/Panel/Item/FavoritePanel.vue";
+  import Icon from "~/components/Symbols/Icon.vue";
+  import CheckBox from "~/components/Input/CheckBox.vue";
+  import Panel from "~/components/Panel/Panel.vue";
+  import PanelItem from "~/components/Panel/PanelItem.vue";
+  import CustomSelect from "~/components/Input/CustomSelect.vue";
+  import TagFilterPanel from "~/components/Panel/Item/TagFilterPanel.vue";
+  import {AxiosError} from "~/node_modules/axios";
+  import {BusEvent} from "~/components/Event/BusEvent";
+
+  @Component({
+    components: {
+      TagFilterPanel,
+      CustomSelect,
+      PanelItem,
+      Panel,
+      FilterPanel,
+      HistoricalPanel,
+      FavoritePanel,
+      Icon,
+      CheckBox
+    },
+    async fetch({app: {$accessor}}) {
+      await $accessor.tags.fetch();
+    },
+    middleware: ['auth', 'admin', 'reset-search-request']
+  })
+  export default class extends Vue {
+    /**
+     * A reference to the input html element for the search
+     */
+    @Ref() inputText!: HTMLInputElement;
+
+    searchModel: string = '';
+
+    get categoriesWithTags(): CategoryWithSelectedTags[] {
+      return this.$accessor.tags.filteredTagByCategories
+    }
+
+    /**
+     * Check if the selected tags array is empty
+     */
+    get isSelectedTagsEmpty() {
+      return this.selectedTags.length === 0
+    }
+
+    get selectedTags(): SelectedTag[] {
+      return this.$accessor.tags.selectedTags
+    }
+
+    get filteredCategoriesWithSearchModel(): CategoryWithSelectedTags[] {
+      return this.searchModel === '' ? this.categoriesWithTags : this.categoriesWithTags.map(categoryWithTags => {
+        const filteredTags: SelectedTag[] = categoryWithTags.tags.filter(tag => {
+          return tag.tag_text.toLowerCase().match(this.searchModel.toLowerCase()) !== null
+        });
+
+        return {
+          ...categoryWithTags,
+          tags: filteredTags
+        } as CategoryWithSelectedTags
+      })
+    }
+
+    /**
+     * Add or remove an id from the selected tags array
+     * Add if state of the checkbox is true and the item is not in the array
+     * Remove if state of the checkbox is false and the item is in the array
+     * @param id
+     * @param state
+     * @param tag
+     */
+    addOrRemoveTags({id, state}: CheckBoxObjectEmitted, tag: SelectedTag) {
+      this.$accessor.tags.addOrRemoveTag({...tag, state})
+    }
+
+    /**
+     * Remove from the databases the exercises and update the search store
+     */
+    async deleteSelectedTags() {
+      try {
+        await this.$axios.$delete('/api/bulk/delete_tags', {data: this.selectedTags.map(tag => tag.tag_id)});
+        BusEvent.$emit('displayNotification', {
+          mode: "success",
+          message: `${this.selectedTags.length} ${this.selectedTags.length === 1 ? 'tag a' : 'tags ont'} été correctement supprimé${this.selectedTags.length === 1 ? '' : 's'}.`
+        });
+        this.$accessor.tags.CLEAR();
+        this.$accessor.tags.fetch();
+      } catch (e) {
+        const error = e as AxiosError;
+
+        BusEvent.$emit('displayNotification', {
+          mode: "error",
+          message: `Une erreur est survenue lors de la suppression.`
+        });
+      }
+    }
+
+    /**
+     * update the state of exercises and notifies the user
+     */
+    async updateStateOfTags(state: boolean) {
+
+      const updateState = (tag: SelectedTag, state: boolean) => {
+        const TagModified: TagExtended = {
+          category_id: tag.category_id,
+          isValidated: state,
+          tag_id: tag.tag_id,
+          tag_text: tag.tag_text,
+          version: tag.version
+        };
+
+        return this.$axios.$put('/api/tags', TagModified)
+      };
+
+      Promise
+        .all(this.selectedTags.map(tag => updateState(tag, state)))
+        .then((response) => {
+          BusEvent.$emit('displayNotification', {
+            mode: "success",
+            message: `${this.selectedTags.length} ${this.selectedTags.length === 1 ? 'tag a' : 'tags ont'} été correctement ${state ? 'validé' : 'invalidé'}${this.selectedTags.length === 1 ? '' : 's'}.`
+          });
+          this.$accessor.tags.CLEAR();
+          this.$accessor.tags.fetch();
+
+        }).catch((error: AxiosError) => {
+        if (error.response) {
+          const status: number = error.response.status;
+
+          if (status === 409) {
+            BusEvent.$emit('displayNotification', {
+              mode: "error",
+              message: `Conflit ! Un tag a déjà été modifié entre temps, vous devez rafraîchir la page.`
+            });
+          }
+        } else {
+          BusEvent.$emit('displayNotification', {
+            mode: "error",
+            message: `Une erreur est survenue. Veuillez-nous en excuser.`
+          });
+        }
+
+      });
+
+
+    }
+
+    /**
+     * Reset the input value
+     */
+    resetInput() {
+      this.inputText.value = ''
+    }
+
+    /**
+     * Go to the exercise with a specific id
+     * @param id
+     */
+    gotoExercise(id: number) {
+      this.$router.push('/administration/exercices/' + id)
+    }
+
+
+    selectAction(action: { content: string, index: number }) {
+
+      if (action.index === 0) {
+        this.updateStateOfTags(true)
+      } else if (action.index === 1) {
+        this.updateStateOfTags(false)
+      } else if (action.index === 2) {
+        this.deleteSelectedTags()
+      }
+
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  @import "../../../assets/css/variables";
+  @import "../../../assets/css/function";
+  @import "../../../assets/css/font";
+  @import "../../../assets/css/table";
+</style>
+
