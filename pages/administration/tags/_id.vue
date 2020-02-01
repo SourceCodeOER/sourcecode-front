e3504acc6249a879bd9dfd2f8ba0cfad9ac39faa
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-02-01 16:00:44 +0100

    refactor(Admin): use TagForm component to extract validation logic

--- pages/administration/tags/_id.vue
+++ pages/administration/tags/_id.vue
@@ -1,14 +1,41 @@
 <template>
   <div class="container--with-menu">
+    <div class="banner banner--with-shadow-bottom">
+      <div class="banner__nav banner__nav--with-link">
+        <span>
+          Administration > Gestion des tags > Créer un tag
+        </span>
+        <nuxt-link to="/administration/tags" tag="span">
+          <Icon type="arrowLeft" class="reversed-arrow" theme="theme--primary-color-light"/>
+          Gestion des tags
+        </nuxt-link>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <TagForm :tag="tag" title="Modifier un tag"/>
+    </div>
 
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator'
+  import TagForm from "~/components/Gestion/TagForm.vue";
+  import Icon from "~/components/Symbols/Icon.vue";
 
   @Component({
-    middleware: ['auth', 'admin', 'reset-search-request']
+    middleware: ['auth', 'admin', 'reset-search-request'],
+    components: {
+      TagForm,
+      Icon
+    },
+    async asyncData({app: {$accessor, $axios}, params}) {
+      await $accessor.tags.fetch();
+      const data = await $axios.$get('/api/tags?tags_ids=' + params.id );
+
+      return {tag:data[0]}
+    }
   })
   export default class extends Vue {
 
