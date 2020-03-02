475a6bd127be99eb5f60501c814de8edc1e88a02
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-03-02 20:37:03 +0100

    feat(types): add User types for request and responses

--- types/user.ts
+++ types/user.ts
@@ -1,8 +1,9 @@
 /**
  * Types for everything related to a User
  */
+import {MetadataSearchExerciseResponse} from "~/types/exercise";
 
-export type UserRole = "user" | "admin";
+export type UserRole = "user" | "admin" | "super_admin";
 export type UserRoleWithGuest = UserRole | "guest";
 
 export interface User {
@@ -13,4 +14,36 @@ export interface User {
 export interface UserInfo extends User {
   role: UserRole,
   id: number
+}
+
+export interface UserInfoWithSelection extends UserInfo {
+  isSelected: boolean
+}
+
+export interface SearchUserRequestMetadata {
+  page?: number,
+  size?: number
+}
+
+export interface MetadataSearchUserResponse extends MetadataSearchExerciseResponse {
+}
+
+export interface SearchUserRequest {
+  metadata?: SearchUserRequestMetadata,
+  roles?: UserRole[],
+  fullName?: string,
+  email?: string
+}
+
+export interface SearchUserResponse {
+  metadata: MetadataSearchUserResponse
+  data: UserInfo[]
+}
+
+export interface PutUserRequest {
+  email?: string,
+  fullName?: string,
+  password?: string,
+  role?: UserRole,
+  id?: number
 }
