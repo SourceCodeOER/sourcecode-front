eb2f00001abfed1046d96aa30238bfe42279575e
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-03-06 14:34:59 +0100

    fix(validator): add new rule url validation to replace wrong regex implementation

--- plugins/validator.js
+++ plugins/validator.js
@@ -1,5 +1,6 @@
 import {localize} from 'vee-validate';
 import fr from 'vee-validate/dist/locale/fr.json'
+import validator from 'validator';
 
 import {extend} from 'vee-validate';
 import {required, email, alpha_spaces, max, min, numeric, regex, mimes, oneOf} from 'vee-validate/dist/rules';
@@ -14,6 +15,17 @@ extend('regex', regex);
 extend('mimes', mimes);
 extend('oneOf', oneOf);
 
+const urlValidation = {
+  validate(value) {
+    if (validator.isURL(value)) {
+      return true;
+    }
+
+    return `Cette URL n'est pas valide`
+  }
+};
+
+extend('url', urlValidation);
 
 export default function ({app}) {
   localize('fr', fr);
