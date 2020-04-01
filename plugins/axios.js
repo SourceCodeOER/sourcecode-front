c4b6e0656d61195c3724c88977bcfeb8c1899b3b
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-04-01 13:25:03 +0200

    feat(Plugin): add interceptor for 401 error (workaround)
    - Transform file to typescript extension
    - Use interceptor to force the user to login w(refresh token workaround)

--- plugins/axios.js
+++
@@ -1,9 +0,0 @@
-import qs from 'qs'
-
-export default function ({ $axios }) {
-  $axios.onRequest(config => {
-    config.paramsSerializer = (params) => {
-      return qs.stringify(params, {arrayFormat: 'repeat'})
-    }
-  });
-}
---
+++ plugins/axios.ts
@@ -0,0 +1,31 @@
+import qs from 'qs'
+import {Plugin} from '@nuxt/types'
+
+const axios: Plugin = ({$axios, redirect, $auth}) => {
+
+  // Set array format to repeat
+  $axios.onRequest(config => {
+    config.paramsSerializer = (params) => {
+      return qs.stringify(params, {arrayFormat: 'repeat'})
+    }
+  });
+
+  // Disconnect user (if connected) when 401 error and redirect to login page
+  $axios.interceptors.response.use(
+    function (response) {
+      return response
+    },
+    async (error) => {
+      if (error.response.status === 401) {
+        if($auth.loggedIn) {
+          await $auth.logout();
+        }
+        redirect(401, "/login");
+      } else {
+        return Promise.reject(error);
+      }
+    });
+
+};
+
+export default axios
