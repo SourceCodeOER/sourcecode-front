a0cca9b878c4bfe4d09655eadc86d9e9a58ff706
alexandre dewit <alexandre.dewit@student.uclouvain.be>
2020-04-02 13:52:19 +0200

    fix(Plugin): use app.router instead of router object (axios)

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,7 @@
 import qs from 'qs'
 import {Plugin} from '@nuxt/types'
 
-const axios: Plugin = ({$axios, redirect, $auth, route}) => {
+const axios: Plugin = ({$axios, redirect, $auth, app}) => {
 
   // Set array format to repeat
   $axios.onRequest(config => {
@@ -16,19 +16,14 @@ const axios: Plugin = ({$axios, redirect, $auth, route}) => {
       return response
     },
     async (error) => {
-      console.log(route.path);
+      if (error.response.status === 401 && app.router && app.router.currentRoute.path !== '/login') {
 
-      if (error.response.status === 401 && route.path !== '/login') {
         if($auth.loggedIn) {
-          console.log('logged');
           await $auth.logout();
         }
 
-        console.log('redirect');
-
         redirect(401, "/login");
       } else {
-        console.log('return error');
         return Promise.reject(error);
       }
     });
